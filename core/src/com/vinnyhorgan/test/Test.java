f22f73cc48690112cf71fa0d5cb292b22de23e3a
Vinny Horgan <vinnyhorgan@gmail.com>
2023-03-07 23:28:27 +0100

    Created basic 3d scene

--- core/src/com/vinnyhorgan/test/Test.java
+++ core/src/com/vinnyhorgan/test/Test.java
@@ -1,31 +1,88 @@
 package com.vinnyhorgan.test;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.utils.Array;
 
 public class Test extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    private PerspectiveCamera camera;
+    private CameraInputController cameraController;
+    private ModelBatch modelBatch;
+    private AssetManager assets;
+    private Array<ModelInstance> instances = new Array<ModelInstance>();
+    private Environment environment;
+    private boolean loading;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create () {
+        camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(10f, 10f, 10f);
+        camera.lookAt(0,0,0);
+        camera.near = 1f;
+        camera.far = 300f;
+        camera.update();
+
+        cameraController = new CameraInputController(camera);
+        Gdx.input.setInputProcessor(cameraController);
+
+        modelBatch = new ModelBatch();
+
+        assets = new AssetManager();
+        assets.load("models/ship.g3db", Model.class);
+        loading = true;
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+    }
+
+    private void doneLoading() {
+        Model ship = assets.get("models/ship.g3db", Model.class);
+
+        for (int x = -10; x <= 10; x++) {
+            for (int z = -10; z <= 10; z++) {
+                ModelInstance shipInstance = new ModelInstance(ship);
+                shipInstance.transform.setToTranslation(x * 10, 0, z * 10);
+                instances.add(shipInstance);
+            }
+        }
+
+        loading = false;
+    }
+
+    @Override
+    public void render () {
+        if (loading && assets.update())
+            doneLoading();
+
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        modelBatch.begin(camera);
+        modelBatch.render(instances, environment);
+        modelBatch.end();
+
+        cameraController.update();
+    }
+
+    @Override
+    public void dispose () {
+        modelBatch.dispose();
+        instances.clear();
+        assets.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+        camera.update();
+    }
 }
--- desktop/src/com/vinnyhorgan/test/DesktopLauncher.java
+++ desktop/src/com/vinnyhorgan/test/DesktopLauncher.java
@@ -6,10 +6,10 @@ import com.vinnyhorgan.test.Test;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("test");
-		new Lwjgl3Application(new Test(), config);
-	}
+    public static void main (String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        config.setTitle("test");
+        new Lwjgl3Application(new Test(), config);
+    }
 }
